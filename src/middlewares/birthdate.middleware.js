7aea5c501be0205d2748d2aabdd03021e82bd4b7
Sebastián Nicolás Gossos Scribe <111235284+WinniGossos33@users.noreply.github.com>
2023-10-05 15:54:43 -0300

    feat: update to delivery

---
+++ src/middlewares/birthdate.middleware.js
@@ -0,0 +1,17 @@
+export const birthday = async( req, res, next ) => {
+    const date = req.body.age; 
+    
+    const today = new Date()
+    const birthdateNum = new Date(date);
+    const age = today.getFullYear() - birthdateNum.getFullYear()
+    const month = today.getMonth() - birthdateNum.getMonth()
+    
+    if( month < 0 || (month === 0 && today.getDate() < birthdateNum.getDate())){
+        age--
+    }
+    req.body.age = age
+
+
+    next()
+
+}
--- src/routers/session.router.js
+++ src/routers/session.router.js
@@ -1,19 +1,42 @@
 import { Router } from "express";
-
+import UserModel from '../dao/models/user.model.js';
+import { birthday } from "../middlewares/birthdate.middleware.js";
 
 const router = Router();
 
-router.post('/login', ( req, res ) => {
-    //Todo
-    const user = req.body
-    res.send('hola estoy en login :)')
+router.post('/login', async( req, res ) => {
+    
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email, password }).lean().exec();
+    if( !user ) {
+        return res.redirect('/')
+    }
+    if( user.email === ' adminCoder@coder.com' && user.password === 'adminCod3r123' ){
+        user.role = 'admin';
+    }  else {
+        user.role = 'user';
+    }
+    req.session.user = user;
+    res.redirect('/products')
+    // res.send('hola estoy en login :)' + `${user}`)
 })
 
-router.post('/register', ( req, res ) => {
+router.post('/register', birthday , async( req, res ) => {
+
+    const userToRegister = req.body;
+    const user = new UserModel( userToRegister )
+    await user.save() 
+    console.log( userToRegister ) 
+    // res.send('estoy en register :)')
+    res.redirect('/')
+})
+
+router.get('/logout', ( req, res ) => {
     //Todo
 
     const user = req.body
-    res.send('hola estoy en register :)')
+    console.log( user )
+    res.send('hola estoy en logout :)' + `${user}`)
 })
 
 
