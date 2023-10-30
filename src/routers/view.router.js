d0733640eeaf0f90605542cb9f0e8846146a4dfa
Sebastián Nicolás Gossos Scribe <111235284+WinniGossos33@users.noreply.github.com>
2023-10-30 20:52:14 -0300

    refactor: Customs Router

--- src/routers/view.router.js
+++ src/routers/view.router.js
@@ -1,17 +1,14 @@
-import { Router } from "express";
+import passport from "passport";
+
+import MyRouter from "./router.js";
+import { ProductManagerDB } from "../dao/db/products_managerDB.js";
 import { getProducts } from "./products.router.js";
 
-import { ProductManagerDB } from "../dao/db/products_managerDB.js";
-
 import { getCarts } from "./cart.router.js";
-import { authToken } from "../utils.js";
-
-import passport from "passport";
-import MyRouter from "./router.js";
 
 const productsManagerDB = new ProductManagerDB();
 
-const router = Router();
+
 
 export default class ViewRouter extends MyRouter {
   init() {
@@ -114,103 +111,3 @@ export default class ViewRouter extends MyRouter {
     
   }
 }
-
-
-//! LOGIN
-// router.get('/', async( req, res ) => {
-//   try{
-//     res.clearCookie('jwt-coder').render('authenticate/login', { userRegister: false })
-//   } catch (err){
-//     res.render('errors/errorAuth', { error: err })
-//   }
-// })
-
-// router.get('/register', async( req, res ) => {
-//   try{
-//     res.render('authenticate/register')
-//   } catch (err){
-//     res.render('errors/errorAuth', { error: err })
-//   }
-// })
-
-// //! PRODUCTS
-// router.get("/products", passport.authenticate('jwt', { failureRedirect:'failToken', session: false}),  async ( req, res ) => {
-
-//   try{
-//     const result = await getProducts( req, res )
-//     const { first_name, last_name, email, age, role, cart } = req.user;
-
-//     res.render("home", { 
-//       products: result.payload,
-//       prevLink: result.prevLink,
-//       nextLink: result.nextLink,
-//       hasPrevPage: result.hasPrevPage,
-//       hasNextPage: result.hasNextPage,
-//       page: result.page,
-//       user: {first_name, last_name, email, age, role, cart},
-//       isAdmin: req.user.role === 'admin'
-//     });
-
-//   } catch (err){
-//     res.render('errors/errorPlatform', { error: err })
-//   }
-// });
-
-// router.get("/products/realtimeproducts", passport.authenticate('jwt', { failureRedirect:'failToken', session: false}), async ( req, res ) => {
-//   try{
-//     const products = await productsManagerDB.getProducts();
-//     res.render("realTimeProducts", { products });
-//   }catch (err){
-//     res.render('errors/errorPlatform', { error: err })
-//   }
-// });
-
-// ! CARTS
-// router.get('/carts/:cid', passport.authenticate('jwt', { failureRedirect:'failToken', session: false}), async( req, res ) => {
-//   try{
-//     const dataCart = await getCarts( req, res );
-//     const cart = JSON.parse(JSON.stringify( dataCart ));
-//     const products = cart.products.map( p => {
-//       const prod = {...p.pId, quantity: p.quantity}
-//       return prod;
-//     })
-    
-//     res.render('cart', { cartId: cart._id, products })
-//   } catch (err) {
-//     res.render('errors/errorPlatform', { error: err })
-//   }
-// })
-
-// //! CHATS
-// router.get('/chat', passport.authenticate('jwt', { failureRedirect:'failToken', session: false}), async( req, res ) => {
-//   try{
-//     res.render('chat')
-//   }catch (err){
-//     res.render('errors/errorPlatform', { error: err })
-//   }
-// })
-
-// //! PROFILE
-// router.get('/profile', passport.authenticate('jwt', { failureRedirect:'failToken', session: false}),  async( req, res ) => {
-//   try{
-
-//     const { first_name, last_name, email, age, cart, role } = req.user;
-//     res.render( 'authenticate/profile', {
-//       first_name,
-//       last_name,
-//       email,
-//       age,
-//       role,
-//       isUser: role === 'user',
-//       cartId: cart
-//     })
-//   } catch (err){
-//     res.render('errors/errorAuth', { error: err })
-//   }
-// })
-
-
-
-// router.get('/failToken', ( req, res ) => res.clearCookie('jwt-coder').render('errors/errorAuth',{error: 'Error to Authenticate'}))
-
-// export default router;
