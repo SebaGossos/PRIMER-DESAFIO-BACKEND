3defb2defe88fbf42fac3800f7e4af5a2afe22b2
Sebastián Nicolás Gossos Scribe <111235284+WinniGossos33@users.noreply.github.com>
2023-11-22 23:26:19 -0300

    refactor: for delivery

--- src/public/products.js
+++ src/public/products.js
@@ -2,10 +2,12 @@
 const callApiAddProd = async( cid , pid ) => {
 
     try{
+        
         await fetch( `/api/carts/${cid}/product/${pid}`, {
             method: 'post'
         })
-        alert(`producto agregado al cartId: ${cid}`)
+        Swal.fire(`Added Successfully!`)
+        // alert(`producto agregado al cartId: ${cid}`)
     } catch ( err ) {
         alert(`Ocurrio un error: ${err.error}`)
     }
