94fe14f13c189cb4c6f774e0dcc4ca7435988c73
Sebastián Nicolás Gossos Scribe <111235284+WinniGossos33@users.noreply.github.com>
2024-01-08 17:36:47 -0300

    update delivery

--- src/routers/cart.router.js
+++ src/routers/cart.router.js
@@ -18,17 +18,15 @@ export default class CartRouter extends MyRouter {
 
     this.put('/:cid', ['USER', 'PREMIUM'], cartController.update)
 
-    this.delete('/:cid', ['PUBLIC'], cartController.delete)
+    this.delete('/:cid', ['ADMIN'], cartController.delete)
 
     this.get('/:cid/purchase', ['USER', 'PREMIUM'], ticketController.purchase)
 
     this.post('/getbill', ['USER', 'PREMIUM'], getBill)
     
     this.post("/:cid/product/:pid", ['USER', 'PREMIUM'], passport.authenticate('jwt', { failureRedirect:'failToken', session: false}), cartController.addToCart)
-    
-    
+      
     this.put('/:cid/product/:pid', ['USER', 'PREMIUM'], cartController.updateQuantity)
-    
     
     this.delete('/:cid/product/:pid', ['PUBLIC'], cartController.deleteProdById)
 
